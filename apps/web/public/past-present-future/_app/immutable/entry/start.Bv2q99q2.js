f1fe1f73d44b18e1bd37822b17bb059f504fc27b
jinhojang6 <jinho5051@gmail.com>
2026-09-18 23:58:14 +0900

    fix(web): restore desktop campaign video

--- apps/web/public/past-present-future/_app/immutable/entry/start.Bv2q99q2.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/BSh_Ic6Q.js";import{y as t}from"../chunks/BRsYEbSU.js";export{t as load_css,r as start};
---
+++ apps/web/public/past-present-future/_app/immutable/entry/start.Bz2eXMmr.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/CMhO4718.js";import{y as t}from"../chunks/BsaywSkX.js";export{t as load_css,r as start};
